5ffdb37b3a4f15aada83baee3d47670b2b3a8ba1
Nemanja <nscepanovic11@gmail.com>
2016-12-06 22:51:24 +0100

    List folder fix

--- src/ftp/FTPClient.java
+++ src/ftp/FTPClient.java
@@ -3,7 +3,6 @@ package ftp;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -16,100 +15,89 @@ import java.util.StringTokenizer;
 
 public class FTPClient {
 
-	private Socket socket;
 	private BufferedReader reader;
-	private BufferedWriter writer;
 	private PrintWriter out;
 	private FileInputStream is;
-	private BufferedInputStream upload;
 	private BufferedReader br;
-	private String host;
-	private int port = 21;
+
+	private static String host;
+	private static int port = 21;
+	private String passHost;
+	private int passPort;
+
 	private String username;
 	private String password;
+
+	private boolean isPassMode = false;
 
 	public FTPClient(Socket socket) throws Exception {
 
 		reader = new BufferedReader(new InputStreamReader(
 				socket.getInputStream()));
-		writer = new BufferedWriter(new OutputStreamWriter(
-				socket.getOutputStream()));
+
 		out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()),
 				true);
+
 		br = new BufferedReader(new InputStreamReader(System.in));
 
 	}
 
 	public void uploadFile() throws Exception {
 		String filename;
+
 		System.out.print("Enter File Path :");
 		filename = br.readLine();
+
 		File f = new File(filename);
 		if (!f.exists()) {
 			System.out.println("File not Exists...");
 			return;
 		}
 
-		// socket = new Socket(host, port);
-
-		String msg;
-		do{
-			 msg = reader.readLine();
-			System.out.println(msg);
-		}while(!msg.startsWith("220 "));
-
-		out.println("USER " + username);
-
-		String response = reader.readLine();
-		System.out.println(response);
-		if (!response.startsWith("331 ")) {
-			throw new IOException(
-					"SimpleFTP received an unknown response after sending the user: "
-							+ response);
-		}
-
-		out.println("PASS " + password);
-
-		response = reader.readLine();
-		System.out.println(response);
-		if (!response.startsWith("230 ")) {
-			throw new IOException(
-					"SimpleFTP was unable to log in with the supplied password: "
-							+ response);
-		}
-
 		is = new FileInputStream(f);
 		BufferedInputStream input = new BufferedInputStream(is);
-		
-		out.println("PASV");
-		response = reader.readLine();
-		System.out.println(response);
-		String ip = null;
-		int port1 = -1;
-		int opening = response.indexOf('(');
-		int closing = response.indexOf(')', opening + 1);
-		if (closing > 0) {
-			String dataLink = response.substring(opening + 1, closing);
-			StringTokenizer tokenizer = new StringTokenizer(dataLink, ",");
-			try {
-				ip = tokenizer.nextToken() + "." + tokenizer.nextToken() + "."
-						+ tokenizer.nextToken() + "." + tokenizer.nextToken();
-				port = Integer.parseInt(tokenizer.nextToken()) * 256
-						+ Integer.parseInt(tokenizer.nextToken());
-			} catch (Exception e) {
-				throw new IOException(
-						"SimpleFTP received bad data link information: "
-								+ response);
+
+		String response;
+		if (!isPassMode) {
+			out.println("PASV");
+			response = reader.readLine();
+			System.out.println(response);
+			if (!response.startsWith("227 ")) {
+				throw new IOException("FTPClient could not request passive mode: "
+						+ response);
 			}
+
+			int opening = response.indexOf('(');
+			int closing = response.indexOf(')', opening + 1);
+			if (closing > 0) {
+				String dataLink = response.substring(opening + 1, closing);
+				StringTokenizer tokenizer = new StringTokenizer(dataLink, ",");
+				try {
+					passHost = tokenizer.nextToken() + "." + tokenizer.nextToken() + "."
+							+ tokenizer.nextToken() + "." + tokenizer.nextToken();
+					passPort = Integer.parseInt(tokenizer.nextToken()) * 256
+							+ Integer.parseInt(tokenizer.nextToken());
+				} catch (Exception e) {
+					throw new IOException(
+							"FTPClient received bad data link information: "
+									+ response);
+				}
+			}
+
+			isPassMode = true;
 		}
 
+		// Send command STOR
 		out.println("STOR " + f.getName());
-		Socket dataSocket = new Socket(ip, port);
-		System.out.println("IP: " + ip);
 
+		// Open data connection
+		Socket dataSocket = new Socket(passHost, passPort);
+
+		// Read command response
 		response = reader.readLine();
 		System.out.println(response);
 
+		// Read data from server
 		BufferedOutputStream output = new BufferedOutputStream(
 				dataSocket.getOutputStream());
 		byte[] buffer = new byte[4096];
@@ -117,87 +105,67 @@ public class FTPClient {
 		while ((bytesRead = input.read(buffer)) != -1) {
 			output.write(buffer, 0, bytesRead);
 		}
+
 		output.flush();
 		output.close();
 		input.close();
+		dataSocket.close();
 
 		response = reader.readLine();
 		System.out.println(response);
 	}
-	/**************************************************************************************/
+
+
 	public void download() throws Exception{
-		
-		String filenameLocal;
-		System.out.print("Enter Local File Path :");
-		filenameLocal = br.readLine();
-		File f = new File(filenameLocal);
-		
-		String filename;
-		System.out.print("Enter File Path :");
-		filename = br.readLine();
-		
-		
-		String msg;
-		do{
-			 msg = reader.readLine();
-			System.out.println(msg);
-		}while(!msg.startsWith("220 "));
 
-		out.println("USER " + username);
+		String outputDir;
+		System.out.print("Enter Output Dir: ");
+		outputDir = br.readLine();
 
-		String response = reader.readLine();
-		System.out.println(response);
-		if (!response.startsWith("331 ")) {
-			throw new IOException(
-					"SimpleFTP received an unknown response after sending the user: "
-							+ response);
-		}
+		String serverFilePath;
+		System.out.print("Enter server file path :");
+		serverFilePath = br.readLine();
 
-		out.println("PASS " + password);
-
-		response = reader.readLine();
-		System.out.println(response);
-		if (!response.startsWith("230 ")) {
-			throw new IOException(
-					"SimpleFTP was unable to log in with the supplied password: "
-							+ response);
-		}
-		
-		
-		
-		out.println("PASV");
-		response = reader.readLine();
-		if (!response.startsWith("227 ")) {
-			throw new IOException("FTPClient could not request passive mode: "
-					+ response);
-		}
-		String ip = null;
-		int port1 = -1;
-		int opening = response.indexOf('(');
-		int closing = response.indexOf(')', opening + 1);
-		if (closing > 0) {
-			String dataLink = response.substring(opening + 1, closing);
-			StringTokenizer tokenizer = new StringTokenizer(dataLink, ",");
-			try {
-				ip = tokenizer.nextToken() + "." + tokenizer.nextToken() + "."
-						+ tokenizer.nextToken() + "." + tokenizer.nextToken();
-				port1 = Integer.parseInt(tokenizer.nextToken()) * 256
-						+ Integer.parseInt(tokenizer.nextToken());
-			} catch (Exception e) {
-				throw new IOException(
-						"FTPClient received bad data link information: "
-								+ response);
+		// Go to passive mode
+		String response;
+		if (!isPassMode) {
+			out.println("PASV");
+			response = reader.readLine();
+			System.out.println(response);
+			if (!response.startsWith("227 ")) {
+				throw new IOException("FTPClient could not request passive mode: "
+						+ response);
 			}
+
+			int opening = response.indexOf('(');
+			int closing = response.indexOf(')', opening + 1);
+			if (closing > 0) {
+				String dataLink = response.substring(opening + 1, closing);
+				StringTokenizer tokenizer = new StringTokenizer(dataLink, ",");
+				try {
+					passHost = tokenizer.nextToken() + "." + tokenizer.nextToken() + "."
+							+ tokenizer.nextToken() + "." + tokenizer.nextToken();
+					passPort = Integer.parseInt(tokenizer.nextToken()) * 256
+							+ Integer.parseInt(tokenizer.nextToken());
+				} catch (Exception e) {
+					throw new IOException(
+							"FTPClient received bad data link information: "
+									+ response);
+				}
+			}
+
+			isPassMode = true;
 		}
-		Socket dataSocket = new Socket(ip, port1);
 
-		
-		out.println("RETR " + filename);
-		response = reader.readLine();
-		System.out.println(response);
+		// Send RETR command
+		out.println("RETR " + serverFilePath);
 
+		// Open data connection
+		Socket dataSocket = new Socket(passHost, passPort);
+
+		// Read data from server
 		BufferedOutputStream output = new BufferedOutputStream(
-				new FileOutputStream(new File(filenameLocal, filename)));
+				new FileOutputStream(new File(outputDir, serverFilePath)));
 		BufferedInputStream input = new BufferedInputStream(
 				dataSocket.getInputStream());
 		byte[] buffer = new byte[4096];
@@ -205,213 +173,127 @@ public class FTPClient {
 		while ((bytesRead = input.read(buffer)) != -1) {
 			output.write(buffer, 0, bytesRead);
 		}
+
 		output.flush();
 		output.close();
 		input.close();
 
-		response = reader.readLine();
-		System.out.println(response);
+		dataSocket.close();
 	}
-	
+
 	public void delete() throws Exception{
 		String filename ;
 		System.out.print("Enter File Path For Delete :");
 		filename = br.readLine();
-		
-		String msg;
-		do{
-			 msg = reader.readLine();
-			System.out.println(msg);
-		}while(!msg.startsWith("220 "));
-		
-		out.println("USER " + username);
 
-		String response = reader.readLine();
-		System.out.println(response);
-		if (!response.startsWith("331 ")) {
-			throw new IOException(
-					"SimpleFTP received an unknown response after sending the user: "
-							+ response);
-		}
-
-		out.println("PASS " + password);
-
-		response = reader.readLine();
-		System.out.println(response);
-		if (!response.startsWith("230 ")) {
-			throw new IOException(
-					"SimpleFTP was unable to log in with the supplied password: "
-							+ response);
-		}
-		
-		
+		// Send DELE command
 		out.println("DELE " + filename);
+
+		String msg;
+		while ((msg = reader.readLine()) != null) {
+			System.out.println(msg);
+			if (msg.startsWith("250")) {
+				break;
+			}
+		}
 	}
-	
+
 	public void makeDir() throws Exception{
 		String dirname ;
 		System.out.print("Enter Dir Name :");
 		dirname = br.readLine();
-		
-		String msg;
-		do{
-			 msg = reader.readLine();
-			System.out.println(msg);
-		}while(!msg.startsWith("220 "));
-		
-		out.println("USER " + username);
 
-		String response = reader.readLine();
-		System.out.println(response);
-		if (!response.startsWith("331 ")) {
-			throw new IOException(
-					"SimpleFTP received an unknown response after sending the user: "
-							+ response);
-		}
-
-		out.println("PASS " + password);
-
-		response = reader.readLine();
-		System.out.println(response);
-		if (!response.startsWith("230 ")) {
-			throw new IOException(
-					"SimpleFTP was unable to log in with the supplied password: "
-							+ response);
-		}
-		
-		
+		// Send MKD command
 		out.println("MKD " + dirname);
+
+		String msg;
+		while((msg = reader.readLine()) != null) {
+			System.out.println(msg);
+			if(msg.startsWith("257")) {
+				break;
+			}
+		}
 	}
-	
-	
+
+
 	public void rename() throws Exception{
 		String oldName ;
 		String newName;
 		System.out.print("Enter Old Name :");
 		oldName = br.readLine();
-		
+
 		System.out.print("Enter New Name :");
 		newName = br.readLine();
-		
-		String msg;
-		do{
-			 msg = reader.readLine();
-			System.out.println(msg);
-		}while(!msg.startsWith("220 "));
-		
-		out.println("USER " + username);
 
-		String response = reader.readLine();
-		System.out.println(response);
-		if (!response.startsWith("331 ")) {
-			throw new IOException(
-					"SimpleFTP received an unknown response after sending the user: "
-							+ response);
-		}
-
-		out.println("PASS " + password);
-
-		response = reader.readLine();
-		System.out.println(response);
-		if (!response.startsWith("230 ")) {
-			throw new IOException(
-					"SimpleFTP was unable to log in with the supplied password: "
-							+ response);
-		}
-		
-		
+		// Send RNFR command
 		out.println("RNFR " + oldName);
 		String msg1 = reader.readLine();
 		System.out.println(msg1);
 
+		// Send RNTO command
 		out.println("RNTO " + newName);
 		msg1 = reader.readLine();
 		System.out.println(msg1);
-		
+
 	}
-	
+
 	public void list() throws Exception{
-		
-		String msg;
-		do{
-			 msg = reader.readLine();
-			System.out.println(msg);
-		}while(!msg.startsWith("220 "));
-		
-		out.println("USER " + username);
+		String response;
 
-		String response = reader.readLine();
-		System.out.println(response);
-		if (!response.startsWith("331 ")) {
-			throw new IOException(
-					"SimpleFTP received an unknown response after sending the user: "
-							+ response);
-		}
-
-		out.println("PASS " + password);
-
-		response = reader.readLine();
-		System.out.println(response);
-		if (!response.startsWith("230 ")) {
-			throw new IOException(
-					"SimpleFTP was unable to log in with the supplied password: "
-							+ response);
-		}
-		
-		
-		out.println("PASV");
-		response = reader.readLine();
-		if (!response.startsWith("227 ")) {
-			throw new IOException("FTPClient could not request passive mode: "
-					+ response);
-		}
-		String ip = null;
-		int port1 = -1;
-		int opening = response.indexOf('(');
-		int closing = response.indexOf(')', opening + 1);
-		if (closing > 0) {
-			String dataLink = response.substring(opening + 1, closing);
-			StringTokenizer tokenizer = new StringTokenizer(dataLink, ",");
-			try {
-				ip = tokenizer.nextToken() + "." + tokenizer.nextToken() + "."
-						+ tokenizer.nextToken() + "." + tokenizer.nextToken();
-				port1 = Integer.parseInt(tokenizer.nextToken()) * 256
-						+ Integer.parseInt(tokenizer.nextToken());
-			} catch (Exception e) {
-				throw new IOException(
-						"FTPClient received bad data link information: "
-								+ response);
+		// Go to passive mode
+		if (!isPassMode) {
+			out.println("PASV");
+			response = reader.readLine();
+			System.out.println(response);
+			if (!response.startsWith("227 ")) {
+				throw new IOException("FTPClient could not request passive mode: "
+						+ response);
 			}
+
+			int opening = response.indexOf('(');
+			int closing = response.indexOf(')', opening + 1);
+			if (closing > 0) {
+				String dataLink = response.substring(opening + 1, closing);
+				StringTokenizer tokenizer = new StringTokenizer(dataLink, ",");
+				try {
+					passHost = tokenizer.nextToken() + "." + tokenizer.nextToken() + "."
+							+ tokenizer.nextToken() + "." + tokenizer.nextToken();
+					passPort = Integer.parseInt(tokenizer.nextToken()) * 256
+							+ Integer.parseInt(tokenizer.nextToken());
+				} catch (Exception e) {
+					throw new IOException(
+							"FTPClient received bad data link information: "
+									+ response);
+				}
+			}
+
+			isPassMode = true;
 		}
-		Socket dataSocket = new Socket(ip, port1);
-		
-		out.println("LIST -a -1");
-		msg = reader.readLine();
-		System.out.println("lista" + msg);
-		msg = reader.readLine();
-		
-		System.out.println("lista" + msg);
-		msg = reader.readLine();
-		System.out.println("lista" + msg);
-		
-		
-		msg = reader.readLine();
-		System.out.println("lista" + msg);
-		
+
+		// Send LIST command
+		out.println("LIST /public_html");
+
+		// Open data connection
+		Socket dataSocket = new Socket(passHost, passPort);
+
+		// Read command response
+		response = reader.readLine();
+		System.out.println(response);
+
+		// Read data from server
+		BufferedReader reader  = new BufferedReader(new InputStreamReader(
+				dataSocket.getInputStream()));
+	    String  line = null;;
+	    while((line = reader.readLine()) != null)
+	    {
+	        System.out.println(line);
+	    }
+	    reader.close();
+	    dataSocket.close();
+
 	}
 
 	public void displayMenu() throws Exception {
-		
-
-		System.out.println("Username");
-		System.out.print("\nEnter Username :");
-		String s = br.readLine();
-		setUsername(s);
-
-		System.out.println("Password");
-		System.out.print("\nEnter Password :");
-		s = br.readLine();
-		setPassword(s);
 
 		while (true) {
 
@@ -443,8 +325,44 @@ public class FTPClient {
 		}
 	}
 
+	public void init() throws Exception {
+		System.out.print("\nEnter Username :");
+		String s = br.readLine();
+		setUsername(s);
+
+		System.out.print("\nEnter Password :");
+		s = br.readLine();
+		setPassword(s);
+
+		String msg;
+		do{
+			msg = reader.readLine();
+			System.out.println(msg);
+		}while(!msg.startsWith("220 "));
+
+		out.println("USER " + username);
+
+		String response = reader.readLine();
+		System.out.println(response);
+		if (!response.startsWith("331 ")) {
+			throw new IOException(
+					"SimpleFTP received an unknown response after sending the user: "
+							+ response);
+		}
+
+		out.println("PASS " + password);
+
+		response = reader.readLine();
+		System.out.println(response);
+		if (!response.startsWith("230 ")) {
+			throw new IOException(
+					"SimpleFTP was unable to log in with the supplied password: "
+							+ response);
+		}
+	}
+
 	public void setHost(String host) {
-		this.host = host;
+		FTPClient.host = host;
 	}
 
 	public void setUsername(String username) {
@@ -455,17 +373,18 @@ public class FTPClient {
 		this.password = password;
 	}
 
-}
 
-class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Host");
 		System.out.print("\nEnter Host :");
-		String h = br.readLine();
-		
-		Socket s = new Socket(h, 21);
-		FTPClient ftp = new FTPClient(s);
+		host = br.readLine();
+
+		Socket socket = new Socket(host, port);
+
+		FTPClient ftp = new FTPClient(socket);
+		ftp.init();
 		ftp.displayMenu();
 	}
+
 }
+
